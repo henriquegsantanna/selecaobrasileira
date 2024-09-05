e974c090c3280e65177c592c1c32905f0fc22738
Henrique Gaiardoni <167096110+henriquegsantanna@users.noreply.github.com>
2024-09-05 15:59:59 -0300

    adicionei todos os jogadores

--- dados.js
+++ dados.js
@@ -1,17 +1,117 @@
 let dados = [
     {
-        titulo : "Neymar",
-        descricao : "Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores jogadores de futebol da atualidade e um dos principais representantes do Brasil no esporte",
-        link : "https://www.instagram.com/neymarjr/"
+        titulo : "Alisson",
+        descricao : "Alisson Ramsés Becker é um dos goleiros mais renomados do futebol mundial. Nascido em Novo Hamburgo, Rio Grande do Sul, o brasileiro se destacou internacionalmente defendendo as cores do Liverpool e da Seleção Brasileira.",
+        link : "https://www.instagram.com/alissonbecker/"
+    },
+    {
+        titulo : "Bento",
+        descricao : "Bento é um nome que tem ganhado cada vez mais destaque no cenário do futebol brasileiro. Apesar de ainda jovem, o goleiro já demonstra um talento promissor e um futuro brilhante pela frente.",
+        link : "https://www.instagram.com/bento_matheus/"
+    },
+    {
+        titulo : "Ederson",
+        descricao : "Ederson Moraes é um dos goleiros mais completos e admirados do futebol mundial. Com um estilo de jogo único, que combina defesas espetaculares com uma habilidade incomum com os pés, o brasileiro se tornou peça fundamental no esquema de jogo do Manchester City e da Seleção Brasileira.",
+        link : "https://www.instagram.com/ederson93/"
+    },
+    {
+        titulo : "William",
+        descricao : "William de Asevedo Furtado é um lateral-direito brasileiro que vem se destacando no cenário nacional, principalmente com a camisa do Cruzeiro. Sua ascensão no futebol tem sido marcada por sua versatilidade, garra e potencial técnico.",
+        link : "https://www.instagram.com/williamw02/"
+    },
+    {
+        titulo : "Guilherme Arana",
+        descricao : "Guilherme Antonio Arana Lopes, mais conhecido como Arana, é um lateral-esquerdo brasileiro que se destaca pela sua força física, velocidade e habilidade técnica. Atualmente, defende as cores do Atlético Mineiro e da Seleção Brasileira.",
+        link : "https://www.instagram.com/guiarana/"
+    },
+    {
+        titulo : "Wendell",
+        descricao : "Wendell Nascimento Borges é um lateral-esquerdo brasileiro que se destacou no futebol europeu e atualmente defende o Porto. Conhecido por sua força física, vigor e qualidade técnica, Wendell conquistou seu espaço no Velho Continente e se tornou um dos principais jogadores da posição em Portugal.",
+        link : "https://www.instagram.com/wendellborges18/"
+    },
+    {
+        titulo : "Danilo",
+        descricao : "Danilo Luiz da Silva, mais conhecido simplesmente como Danilo, é um defensor brasileiro que se destaca pela sua versatilidade e experiência. Atualmente, joga pela Juventus, mas já teve passagens por grandes clubes como Real Madrid e Manchester City.",
+        link : "https://www.instagram.com/daniluiz2/"
+    },
+    {
+        titulo : "Beraldo",
+        descricao : "Lucas Lopes Beraldo é um zagueiro brasileiro que tem se destacado no cenário do futebol mundial. Com apenas 20 anos, o jovem defensor já chama a atenção por sua maturidade, técnica e potencial. Atualmente, veste a camisa do Paris Saint-Germain.",
+        link : "https://www.instagram.com/lberaldo_/"
+    },
+    {
+        titulo : "Éder Militão",
+        descricao : "Éder Gabriel Militão é um dos zagueiros mais promissores e talentosos do futebol mundial. Com uma ascensão meteórica, o brasileiro se consolidou como peça fundamental na defesa do Real Madrid e da Seleção Brasileira.",
+        link : "https://www.instagram.com/edermilitao/"
+    },
+    {
+        titulo : "Gabriel Magalhães",
+        descricao : "Gabriel dos Santos Magalhães é um zagueiro brasileiro que tem se destacado no futebol europeu, especialmente com a camisa do Arsenal. Sua imponência física, qualidade técnica e liderança em campo o transformaram em um dos pilares da defesa dos Gunners.",
+        link : "https://www.instagram.com/_gabrielmagalhaes/"
+    },
+    {
+        titulo : "Marquinhos",
+        descricao : "Marcos Aoás Corrêa, mais conhecido como Marquinhos, é um dos zagueiros mais completos e influentes do futebol mundial. Atualmente, veste a camisa do Paris Saint-Germain e da Seleção Brasileira, onde se destaca por sua liderança, qualidade técnica e versatilidade.",
+        link : "https://www.instagram.com/marquinhosm5/"
+    },
+    {
+        titulo : "André",
+        descricao : "André Trindade da Costa Neto, mais conhecido apenas como André, é um jovem volante brasileiro que vem se destacando no futebol internacional. Após uma passagem de sucesso pelo Fluminense, onde se consolidou como um dos principais jogadores da equipe, o volante foi transferido para o Wolverhampton Wanderers, da Premier League.",
+        link : "https://www.instagram.com/andre7/"
+    },
+    {
+        titulo : "Bruno Guimarães",
+        descricao : "Bruno Guimarães Rodrigues Moura é um dos volantes mais talentosos e promissores do futebol brasileiro. Sua habilidade, visão de jogo e energia em campo o tornaram peça fundamental tanto no Newcastle United quanto na Seleção Brasileira.",
+        link : "https://www.instagram.com/brunoguimaraes/"
+    },
+    {
+        titulo : "Gerson",
+        descricao : "Gerson Santos da Silva, mais conhecido simplesmente como Gerson, é um dos meio-campistas mais talentosos e versáteis do futebol brasileiro. Atualmente, veste a camisa do Flamengo e da Seleção Brasileira, onde se destaca por sua técnica apurada, visão de jogo e personalidade forte.",
+        link : "https://www.instagram.com/gersonsantoss/"
+    },
+    {
+        titulo : "João Gomes",
+        descricao : "João Gomes é um talentoso meio-campista brasileiro que vem se destacando no futebol europeu, mais precisamente no Wolverhampton Wanderers. Sua força física, marcação implacável e habilidade para distribuir o jogo o tornaram um dos pilares da equipe inglesa.",
+        link : "https://www.instagram.com/joaogomes.01/"
+    },
+    {
+        titulo : "Lucas Paquetá",
+        descricao : "Lucas Tolentino Coelho de Lima, mais conhecido como Lucas Paquetá, é um dos meio-campistas mais talentosos e promissores do futebol brasileiro. Sua habilidade, visão de jogo e versatilidade o tornaram um dos pilares da Seleção Brasileira e, atualmente, do West Ham United.",
+        link : "https://www.instagram.com/lucaspaqueta/"
+    },
+    {
+        titulo : "Rodrygo",
+        descricao : "Rodrygo Silva de Goes é um dos jovens talentos mais brilhantes do futebol mundial. Atacante de extrema habilidade e velocidade, o brasileiro tem encantado a todos com suas atuações pelo Real Madrid e pela Seleção Brasileira.",
+        link : "https://www.instagram.com/rodrygogoes/"
     },
     {
         titulo : "Endrick",
-        descricao : "Endrick Felipe Moreira de Sousa é um jovem atacante brasileiro que vem chamando a atenção do mundo do futebol por sua precocidade e talento",
+        descricao : "Endrick Felipe Moreira de Sousa é um jovem atacante brasileiro que vem chamando a atenção do mundo do futebol por sua precocidade e talento.",
         link : "https://www.instagram.com/endrick/"
     },
     {
+        titulo : "Estêvão",
+        descricao : "Estevão é um nome que tem ganhado cada vez mais destaque no cenário do futebol brasileiro. Aos poucos, o jovem jogador tem se destacado e chamado a atenção de grandes clubes e da torcida.",
+        link : "https://www.instagram.com/estevaowilian_/"
+    },
+    {
+        titulo : "Luiz Henrique",
+        descricao : "Luiz Henrique André Rosa da Silva é um dos nomes mais promissores do futebol brasileiro. Atacante rápido, habilidoso e com um faro de gol apurado, o jogador tem se destacado com a camisa do Botafogo.",
+        link : "https://www.instagram.com/luiz_henriqueee_11/"
+    },
+    {
+        titulo : "João Pedro",
+        descricao : "João Pedro Junqueira Jesus é um atacante brasileiro que tem se destacado no futebol europeu, atualmente vestindo a camisa do Brighton & Hove Albion. Com sua força física, habilidade e faro de gol, o jogador tem se tornado uma das grandes promessas do futebol brasileiro.",
+        link : "https://www.instagram.com/joaopedro.oficial/"
+    },
+    {
         titulo : "Lucas Moura",
-        descricao : "Lucas Rodrigues Moura da Silva é um jogador de futebol brasileiro conhecido por sua versatilidade e habilidade em campo. Atualmente defendendo as cores do São Paulo Futebol Clube, Lucas já teve uma trajetória de sucesso na Europa, principalmente no Tottenham Hotspur",
+        descricao : "Lucas Rodrigues Moura da Silva é um jogador de futebol brasileiro conhecido por sua versatilidade e habilidade em campo. Atualmente defendendo as cores do São Paulo Futebol Clube, Lucas já teve uma trajetória de sucesso na Europa, principalmente no Tottenham Hotspur.",
         link : "https://www.instagram.com/lucasmoura7/"
+    },
+    {
+        titulo : "Vinicius Junior",
+        descricao : "Vinícius José Paixão de Oliveira Júnior, mais conhecido como Vinícius Júnior, é um dos maiores talentos do futebol mundial. Atacante rápido, habilidoso e com um drible desconcertante, o brasileiro tem se destacado com a camisa do Real Madrid e da Seleção Brasileira.",
+        link : "https://www.instagram.com/vinijr/"
     }
 ];
